893fea13846f420cffd6cba7abd2dc15b90409c6
kismatkc <kismatkc28@gmail.com>
2024-06-11 03:41:35 -0400

    Auto-commit

--- app/api/auth/[…nextauth]/route.ts
+++ app/api/auth/[…nextauth]/route.ts
@@ -1,22 +1,20 @@
-import NextAuth from 'next-auth';
-import CredentialsProvider from "next-auth/providers/credentials"
-
-
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = NextAuth({
-
-session:{
-  strategy: "jwt"
-},
-  providers:[
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
     CredentialsProvider({
       name: "Credentials",
-      credentials:{
-        email: {label: "Email",type: "email"}
-      }
-    })
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+    }),
   ],
-  pages:{
-    signIn: "/auth/signin"
-  }
-})
+  pages: {
+    signIn: "/auth/signin",
+  },
+});
