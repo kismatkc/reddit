2a5123d32bd918cdff805e9e7b88b9204fce1db5
kismatkc <kismatkc28@gmail.com>
2024-06-12 05:32:54 -0400

    Auto-commit

--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -5,11 +5,16 @@ const {
   createPost,
 } = require("../controllers/postsController");
 
+const { createUser } = require("../controllers/users-controllers");
+
 const router = express.Router();
 
-// Routes
+// Routes for posts
 router.get("/public", getPublicPosts);
 router.get("/search", getSearchPosts);
 router.post("/createPost", createPost);
+
+//Routes for users
+router.post("/createUser", createUser);
 
 module.exports = router;
