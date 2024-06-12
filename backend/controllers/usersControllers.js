e09e0742fd719584b7f306e919ff1d77c9dcf299
kismatkc <kismatkc28@gmail.com>
2024-06-12 06:03:04 -0400

    Auto-commit

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -4,18 +4,5 @@ export const createUser = async (req, res) => {
   await connectDatabase();
   try {
     const { email, password } = req.body;
-    if (!email || !password) {
-      res.status(400).send("Email and password are required");
-      return;
-    }
-
-    // Assuming you have a User model
-    const newUser = new User({ email, password });
-    await newUser.save();
-
-    res.status(201).send(newUser);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Internal server error");
-  }
+  } catch (err) {}
 };
