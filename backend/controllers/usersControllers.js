73b8188022835255fb54beac1427e9c8e734f4bb
kismatkc <kismatkc28@gmail.com>
2024-06-12 06:03:36 -0400

    Auto-commit

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,8 +1,13 @@
+import Post from "../models/Post";
 import connectDatabase from "../server";
 
 export const createUser = async (req, res) => {
   await connectDatabase();
   try {
     const { email, password } = req.body;
+    const newUser = await Post({
+      email,
+      password,
+    });
   } catch (err) {}
 };
